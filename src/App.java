fca4113e9e53687d38493ba1ef8eacd581f3d6b0
Veronica Cobos <vcobos178@gmail.com>
2024-10-02 12:13:23 -0500

    02/10

--- src/App.java
+++ src/App.java
@@ -2,8 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         MetodoOrdenamiento ordenar = new MetodoOrdenamiento();
         int[] arreglo = {48, 2, 8, 27, 12};
-        int[] arregloInsercion = ordenar.sortInsercion(arreglo);
+        int[] arregloInsercion = ordenar.sortInsercion(arreglo, false);
         ordenar.printInsercion(arregloInsercion);
-
     }
 }
--- src/MetodoOrdenamiento.java
+++ src/MetodoOrdenamiento.java
@@ -1,13 +1,32 @@
+import java.util.Arrays;
+
 public class MetodoOrdenamiento {
-    public int[] sortInsercion(int[] arreglo){
-        for(int i = 1; i < arreglo.length; i++){
-            int aux = arreglo[i];
-            int j = i - 1;
-            while(j >= 0 &&arreglo[j] > aux ){
-                arreglo [j + 1] = arreglo[j];
-                j--;
+    public int[] sortInsercion(int[] arreglo, boolean logs){
+        if(logs == true){
+            for(int i = 1; i < arreglo.length; i++){
+                System.out.println("Iteracion numero " + i);
+                int aux = arreglo[i];
+                int j = i - 1;
+                System.out.println("\ti=" + i + " j=" + j + " [i]=" + arreglo[i] + " [j]=" + arreglo[j]);
+                while(j >= 0 && arreglo[j] > aux ){
+                    System.out.println("\t\tComparamos " + aux + " con " + arreglo[j]);
+                    arreglo [j + 1] = arreglo[j];
+                    j--;
+                    System.out.println("\t\t--------" + Arrays.toString(arreglo));
+                }
+                arreglo[j+1] = aux;
+                System.out.println("\t--------" + Arrays.toString(arreglo));
             }
-            arreglo[j+1] = aux;
+        } else {
+            for(int i = 1; i < arreglo.length; i++){
+                int aux = arreglo[i];
+                int j = i - 1;
+                while(j >= 0 && arreglo[j] > aux ){
+                    arreglo [j + 1] = arreglo[j];
+                    j--;
+                }
+                arreglo[j+1] = aux;
+            }
         }
         return arreglo;
     }
